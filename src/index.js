546d423cc350900be25bbbc8415bcf5f744124b3
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-21 16:52:13 +0000

    add recursive tree building function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { ro } from 'date-fns/locale';
 import './style.css';
 
 class Node {
@@ -16,6 +17,17 @@ class Tree {
       if (array[i] === array[i + 1])
         throw new Error('array cannot have duplicates');
 
-    function resursiveBuildTree(array, start, end) {}
+    function recursiveBuildTree(array, start, end) {
+      if (start > end) return null;
+
+      let mid = Math.floor((end - start) / 2);
+
+      let root = new Node(array[mid]);
+
+      root.left = recursiveBuildTree(array, start, array[mid] - 1);
+      root.right = recursiveBuildTree(array, array[mid] + 1, end);
+
+      return root;
+    }
   }
 }
