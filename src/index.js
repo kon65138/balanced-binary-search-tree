71a9d9f667eb8faeeca6e6b63baded7285a818bb
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-21 16:44:37 +0000

    add array duplicate detection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './style.css';
 
-class node {
+class Node {
   constructor(data, left, right) {
     this.data = data;
     this.left = left;
     this.right = right;
+  }
+}
+
+class Tree {
+  constructor(array) {}
+
+  buildTree(array) {
+    for (let i = 0; i < array.length; i++)
+      if (array[i] === array[i + 1])
+        throw new Error('array cannot have duplicates');
+
+    function resursiveBuildTree(array, start, end) {}
   }
 }
